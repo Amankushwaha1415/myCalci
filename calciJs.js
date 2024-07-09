5ba48ffdc3710576a81b327ecc926d4cb504a51c
Amankushwaha1415 <143944206+Amankushwaha1415@users.noreply.github.com>
2024-07-09 21:34:17 +0530

    Add files via upload

---
+++ calciJs.js
@@ -0,0 +1,41 @@
+let screenData="";
+let btns=document.querySelectorAll
+("button");
+let inputdata=document.querySelector("input");
+console.log(inputdata);
+console.log(screenData);
+console.log(btns);
+
+btns.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+        if(btn.innerText==="="){
+            let idx=screenData.indexOf("%*");
+            console.log(idx);
+            if(idx>0&&idx<screenData.length-1){
+                let num1=Number(screenData.slice(0,idx));
+                console.log(num1);
+                let num2=Number(screenData.slice(idx+2,screenData.length));
+                console.log(num2);
+                let output=(num1/100)*num2;
+                console.log(output);
+                inputdata.value=output;
+            }
+            else{
+                screenData=eval(screenData);
+                inputdata.value=screenData;
+            }
+        }
+        else if(btn.innerText==="C"){
+            screenData="";
+            inputdata.value=screenData;
+        }
+        else if(btn.innerText==="<--"){
+            screenData=screenData.slice(0,-1);
+            inputdata.value=screenData;
+        }
+        else{
+            screenData=screenData+btn.innerText;
+            inputdata.value=screenData;
+        }
+    });
+});
